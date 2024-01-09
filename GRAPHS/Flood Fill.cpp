59e14d19827128589efa18b39d4903234c087d0b
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-09 09:21:30 +0530

    Flood Fill code added

--- GRAPHS/Flood Fill.cpp
+++ GRAPHS/Flood Fill.cpp
@@ -38,3 +38,28 @@ int main()
     
     return 0;
 }
+
+/*
+vector<vector<int> > floodFill(vector<vector<int> >&image, int sr, int sc, int color){
+    if(color == image[sr][sc]) return image;
+    dfs(image, sr, sc, image[sr][sc], color);
+    return image;
+}
+void dfs(vector<vector<int> >&image, int currRow, int currCol, int initialCOlor, int newColor){
+    int n = image.size();
+    int m = image[0].size();
+
+    if(currRow < 0 || currCol < 0 || currRow >= n || currCol >= m){
+        return ;
+    }
+
+    if(image[currRow][currCol] != initialColor) return;
+
+    image[currRow][currCol] = newColor;
+    dfs(image, currRow+1, currCol, initialColor, newColor); // down
+    dfs(image, currRow, currCol-1, initialColor, newColor); // left
+    dfs(image, currRow-1, currCol, initialColor, newColor); // up
+    dfs(image, currRow, currCol+1, initialColor, newColor); // right
+}
+
+*/
