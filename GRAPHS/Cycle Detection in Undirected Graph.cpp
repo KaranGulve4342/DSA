a4e07966b922542111b4aaea026a893495ed6ddd
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-12 00:04:50 +0530

    Detect cycle in undirected graphs ( DFS + BFS)

--- GRAPHS/Cycle Detection in Undirected Graph.cpp
+++ GRAPHS/Cycle Detection in Undirected Graph.cpp
@@ -204,3 +204,67 @@ string cycleDetection(vector<vector<int>>&edges, int n, int m){
 }
 
 */
+
+// USING BFS
+
+/*
+bool detect(int src, vector<int> adj[], int vis[]){
+    vis[src] = 1;
+    queue<pair<int, int> >q;
+    q.push({src, -1});
+    while(!q.empty()){
+        int node = q.front().first;
+        int parent = q.front().second;
+        q.pop();
+
+        for(auto adjacentNode : adj[node]){
+            if(!vis[adjacentNode]){
+                vis[adjacentNode] = 1;
+                q.push({adjacentNode, node});
+            }
+            else if(parent != adjacentNode){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool isCycle(int V, vector<int> adj[]){
+    int vis[V] = {0};
+    for(int i = 0;i < V;i++){
+        if(!vis[i]){
+            if(detect(i, adj, vis)) return true;
+        }
+    }
+    return false;
+}
+*/
+
+// USING DFS
+/*
+class Solution{
+private:
+    bool dfs(int node, int parent, vector<int> vis[], vector<int> adj[]){
+        vis[node] = 1;
+        for(auto adjacentNode : adj[Node]){
+            if(!vis[adjacentNode]){
+                if(dfs(adjacentNode, node, vis, adj) == true){
+                    return true;
+                }
+            }
+            else if(adjacentNode != parent) return true;
+        }
+        return false;
+    }
+
+    bool isCycle(int V, vector<int> adj[]){
+        int vis[V] = {0};
+        for(int i = 0;i < V;i++){
+            if(!vis[i]){
+                if(dfs(i, -1, vis, adj) == true) return true;
+            }
+        }
+        return false;
+    }
+};
+*/
