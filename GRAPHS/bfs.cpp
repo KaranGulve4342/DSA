01bc540d531c2d2fc21b24ac331dd37b01b528c7
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-11 08:31:20 +0530

    BFS code added striever

--- GRAPHS/bfs.cpp
+++ GRAPHS/bfs.cpp
@@ -1,3 +1,39 @@
+/*
+
+class Solution{
+public:
+    vector<int> bfsOfGraph(int V, vector<int> adj[]){
+        int visited[n] = {0};
+        visited[0] = 1;
+        queue<int> q;
+        q.push(0);
+        vector<int> bfs;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push(node);
+
+            for(auto it : adj[node]){
+                if(!visited[i]){
+                    visited[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
+    }
+}
+
+*/
+
+
+
+
+
+
+
+
+
 #include<iostream>
 #include<vector>
 #include<list>
