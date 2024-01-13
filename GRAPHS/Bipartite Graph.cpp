426dc5d8880f36855cce1f4cf47b43aff4932239
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-13 15:38:07 +0530

    commited changes

--- GRAPHS/Bipartite Graph.cpp
+++ GRAPHS/Bipartite Graph.cpp
@@ -51,3 +51,50 @@ class Solution{
         return true;
     }
 }
+
+/*
+bool check(int start, int V, vector<int> adj[], int color[]){
+    queue<int> q;
+    q.push(start);
+    color[start] = 0;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+
+        for(auto it : adj[node]){
+            // if the adjacent node is not yet colored
+            // you will give the opposite color of the node
+
+            if(color[it] == -1){
+                color[it] = !color[node];
+                q.push(it);
+            }
+
+            // is the adjacent guy having the same color
+            // someone did color it on some other path
+
+            else if(color[it] == color[node]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool is Bipartite(int V, vector<int> adj[]){
+    int color[V];
+    for(int i = 0;i < V;i++){
+        color[i] = -1;
+    }
+
+    for(int i = 0;i < V;i++){
+        if(color[it] == -1){
+            if(check(i, V, adj, color) == false){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+*/
