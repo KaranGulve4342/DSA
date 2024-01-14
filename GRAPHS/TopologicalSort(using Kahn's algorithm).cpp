c3489344692eb34d6fe5a0de4a4ec5624cb832b6
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-15 02:19:01 +0530

    Prerequisites tasks added

--- GRAPHS/TopologicalSort(using Kahn's algorithm).cpp
+++ GRAPHS/TopologicalSort(using Kahn's algorithm).cpp
@@ -84,3 +84,40 @@ vector<int> topoSort(int V, vector<int> adj[]){
     return topo;
 }
 */
+
+/*
+class Solution{
+public:
+    // function to detect cycle in a directed graph
+    bool iszCyclic(int V, vector<int> adj[]){
+        int indegree[V] = {0};
+        for(int i = 0;i < V;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i = 0;i < V;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+
+        int count = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            count++;
+
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+
+        if(count == V) return false;
+        return true;
+    }
+}
+*/
