83a1c04680d7224e2a4ef7aa55defc9f63ec6246
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-14 10:48:22 +0530

    Topological Sort added

--- GRAPHS/Topological Sort(using DFS).cpp
+++ GRAPHS/Topological Sort(using DFS).cpp
@@ -83,3 +83,37 @@ vector<int> topologicalSort(vector<vector<int> >&edges, int v, int e){
     return ans;
 }
 */
+
+// GFG
+/*
+class Solution {
+    void dfs(int node, int vis[], stack<int> &st, vector<int> adj[]){
+        vis[node] = 1;
+
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it, vis, st, adj);
+            }
+        }
+        st.push(node);
+    }
+
+    vector<int> topoSort(int V, vector<int> adj[]){
+        int vis[V] = 0;
+        stack<int> st;
+        for(int i = 0;i < V;i++){
+            if(!vis[i]){
+                dfs(i, vis, st, adj);
+            }
+        }
+
+        vector<int> ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        return ans;
+    }
+}
+*/
