b656ee795307a7abf7e42027a758df60361e529b
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-15 20:08:14 +0530

    commited changes

--- DYNAMIC PROGRAMMING/Longest Increasing Subsequence.cpp
+++ DYNAMIC PROGRAMMING/Longest Increasing Subsequence.cpp
@@ -313,3 +313,20 @@ int longestSubsequence(int n, int a[]){
 }
 */
 
+// longest Increasing Subsequence
+/*
+int solve(int ind, int prev_ind, int arr[], int n, vector<vector<int> > &dp){
+    if(ind == n) return 0;
+    if(dp[ind][prev_ind+1] != -1){
+        return dp[ind][prev_ind+1];
+    }
+    int len = 0 + solve(ind + 1, prev_ind, arr, n, dp);
+    if(prev_ind == -1 || arr[ind] > arr[prev_ind]){
+        len = max(len, 1 + solve(ind + 1, ind, arr, n, dp));
+    }
+}
+int longestIncreasingSubsequence(int arr[], int n){
+    vector<vector<int> > dp(, vector<int>(n+1, -1));
+    return solve(0, -1, arr, n, dp);
+}
+*/
