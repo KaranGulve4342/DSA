a71eff7f12c6b9ed6bc88a0c90962bc295be6af0
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-17 22:13:24 +0530

    code changes

--- GRAPHS/Dijkstra's Algorithm.cpp
+++ GRAPHS/Dijkstra's Algorithm.cpp
@@ -58,3 +58,41 @@ vector<int> dijkstra(vector<vector<int> >&vec, int vertices, int edges, int sour
     return dist;
 
 }
+
+/*
+
+class Solution{
+public:
+    // Funtion to find the shortest distance of all the vertices from the source vertex S.
+
+    vector<int> dijkstra(int V, vector<vector<int> >adj[], int S){
+        priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int>>> pq;
+        vector<int> dist(V);
+        for(int i = 0;i < V;i++){
+            dist[i] = 1e9;
+        }
+
+        dist[S] = 0;
+        pq.push({0, S});
+
+        while(!pq.empty()){
+            int dis = pq.top().first;
+            int node = pq.top().first;
+            pq.pop();
+
+            for(auto it : adj[node]){
+                int edgeWeight = it[1];
+                int adjNode = it[0];
+
+                if(dis + edgeWeight < dist[adjNode]){
+                    dist[adjNode] = dist + edgeWeight;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+
+        return dist;
+    }
+}
+
+*/
