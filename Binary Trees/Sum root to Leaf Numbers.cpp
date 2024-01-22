6f27021b7eed4f198fb863acf49918f7ddddcf9f
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-22 09:27:48 +0530

    Sum root to leaf number code added

---
+++ Binary Trees/Sum root to Leaf Numbers.cpp
@@ -0,0 +1,38 @@
+// LEETCODE 129
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    void dfs(TreeNode* root, long long val){
+        if(root == NULL) return;
+
+        val *= 10;
+        val += root->val;
+
+        if(!root->right && !root->left){
+            ans += val;
+            return;
+        }
+
+        dfs(root->left, val);
+        dfs(root->right, val);
+
+    }
+    int sumNumbers(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        dfs(root, 0);
+        return ans;
+    }
+};
