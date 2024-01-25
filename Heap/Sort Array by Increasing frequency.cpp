1f97971f0f1a22af28ef241cb4f4e17311580378
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-25 22:09:46 +0530

    Sort Array by Increasing frequency code added ❤

--- Heap/Sort Array by Increasing frequency.cpp
+++ Heap/Sort Array by Increasing frequency.cpp
@@ -1,2 +1,31 @@
 // LEETCODE 1636
 
+class Solution {
+public:
+    typedef pair<int, int> pi;
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> umap;
+
+        for(int num : nums){
+            umap[num]++;
+        }
+
+        priority_queue<pi, vector<pi>, greater<pi>> pq;
+
+        for(auto x : umap){
+            pq.push({-x.second, x.first});
+        }
+
+        vector<int> ans;
+        while(pq.size() > 0){
+            int freq = pq.top().first;
+            int num = pq.top().second;
+            while(freq++){
+                ans.push_back(num);
+            }
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
