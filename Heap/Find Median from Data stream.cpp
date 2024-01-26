ee7c3a0dcbbb73b756858216a9ec29d5706324a8
KaranGulve4342 <karangulve4342@gmail.com>
2024-01-26 09:29:51 +0530

    COmmited changes

---
+++ Heap/Find Median from Data stream.cpp
@@ -0,0 +1,41 @@
+// LEETCODE 295
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    priority_queue<int> left; // maxHeap
+    priority_queue<int, vector<int>, greater<int> >right; // minHeap
+
+    MedianFinder() {
+
+    }
+
+    void addNum(int num){
+        if(left.size() == 0 || nums < left.top()){
+            left.push(num);
+        }
+        else{
+            right.push(num);
+        }
+
+        if(left.size() > right.size() + 1){
+            right.push(left.top());
+            left.pop();
+        }
+        if(right.size() > left.size() + 1){
+            left.push(right.top());
+            right.pop();
+        }
+    }
+
+    double findMedian() {
+        if(left.size() == right.size()){
+            return (left.top() + right.top())/2.0;
+        }
+        else{
+            return right.top();
+        }
+    }
+}
