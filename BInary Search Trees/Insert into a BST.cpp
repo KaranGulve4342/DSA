54db575a1def498a704b0807ee91ad8f43bccc48
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-02 17:57:58 +0530

    Commited changes

--- BInary Search Trees/Insert into a BST.cpp
+++ BInary Search Trees/Insert into a BST.cpp
@@ -24,6 +24,31 @@ class Solution{
 }
 */
 
+/*
+class Solution{
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val){
+        if(root == NULL) return new TreeNode(val);
+        else if(root->val > val){
+            if(root->left == NULL){
+                root->left = new TreeNode(val);
+            }
+            else{
+                insertIntoBST(root->left, val);
+            }
+        }
+        else{
+            if(root->right == NULL){
+                root->right = new TreeNode(val);
+            }
+            else insertIntoBST(root->right, val);
+        }
+
+        return root;
+    }
+}
+*/
+
 
 /*
 class Solution{
