8e9ec5a698e21347ef2a41db4c91633602eddc12
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-02 20:01:21 +0530

    Convert sorted Array to BST

--- BInary Search Trees/Validate BST.cpp
+++ BInary Search Trees/Validate BST.cpp
@@ -1,4 +1,22 @@
 // LEETCODE 98
+/*
+long long maxTree(TreeNode* root){
+    if(root == NULL) return LLONG_MIN;
+    return max((long long)(root->val), max(maxTree(root->left), maxTree(root->right)));
+}
+long long minTree(TreeNode* root){
+    if(root == NULL) return LLONG_MAX;
+    return min((long long)(root->val), min(minTree(root->left), minTree(root->right)));
+}
+bool isValidBST(TreeNode* root){
+    if(root == NULL) return true;
+    else if((long long)(root->val) <= maxTree(root->left)) return false;
+    else if((long long)(root->val) >= minTree(root->right)) return false;
+    return isValidBST(root->left) && isValidBST(root->right);
+}
+*/
+
+
 
 /*
 class Solution{
@@ -46,14 +64,14 @@ class Solution{
 /*
 class Solution{
     public:
-        TreeNode* prev == NULL;
+        TreeNode* prev = NULL;
         bool flag = true;
         void inorder(TreeNode* root){
             if(root == NULL) return;
-            inorder(root->left);
+            inprder(root->left);
             if(prev != NULL){
                 if(root->val <= prev->val){
-                    flag == false;
+                    flag = true;
                     return;
                 }
             }
@@ -61,9 +79,7 @@ class Solution{
             inorder(root->right);
         }
         bool isValidBST(TreeNode* root){
-            vector<int> ans;
-            inorder(root, ans);
-            
+            inorder(root);
             return flag;
         }
 };
