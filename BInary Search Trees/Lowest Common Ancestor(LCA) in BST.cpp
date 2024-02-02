829036fb968ff4695e26ab14ee901bbadbb03773
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-02 19:13:02 +0530

    LCA of BST added

--- BInary Search Trees/Lowest Common Ancestor(LCA) in BST.cpp
+++ BInary Search Trees/Lowest Common Ancestor(LCA) in BST.cpp
@@ -1,5 +1,16 @@
 
 /*
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+    if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right, p, q);
+    else if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+    else return root;
+}
+
+*/
+
+
+/*
 class Solution{
     TreeNode* lowestCommonAncestor<TreeNode* root,TreeNode* p,TreeNode* q){
         if(root->val == p->val || root->val == q->val) return root;
