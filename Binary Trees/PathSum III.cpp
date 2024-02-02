edafc516a95cb2d4c6cf9912f3418b283071eb72
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-02 08:19:02 +0530

    comited

--- Binary Trees/PathSum III.cpp
+++ Binary Trees/PathSum III.cpp
@@ -23,3 +23,20 @@ class Solution{
     }
 };
 */
+
+/*
+void helper(TreeNode* root, long long sum, int &count){
+    if(root == NULL) return;
+    if((long long)(root->val) == sum) count++;
+    helper(root>left, sum-(long long)(root->val), count);
+    helper(root->right, sum-(long long)(root->val), count);
+}
+int pathSum(TreeNode* root, int targetSum){
+    if(root == NULL) return;
+    if count = 0;
+    helper(root, (long long)(targetSum), count);
+    count += (pathSum(root->left, targetSum) + pathSum(root->right, targetSum));
+    return count;
+
+}
+*/
