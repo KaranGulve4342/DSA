4a8f8afaa6f13e75a3c20c68215cf61fedf6fb37
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-02 07:56:23 +0530

    Path Sum II code added

--- Binary Trees/Path Sum II.cpp
+++ Binary Trees/Path Sum II.cpp
@@ -25,3 +25,31 @@ class Solution{
     }
 }
 */
+
+/*
+class Solution{
+public:
+    void helper(TreeNode* root, vector<int>v, vector<vector<int> >&ans, int sum){
+        if(root == NULL) return;
+        
+        if(root->left == NULL && root->right == NULL){
+            if(root->val == sum){
+                v.push_back(root->val);
+                ans.push_back(v);
+            }
+            return;
+        }
+
+
+        v.push_back(root->val);
+        helper(root->left, v, ans, targetSum - (root->val));
+        helper(root->right, v, ans, targetSum - (root->val));
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum){
+        vector<vector<int> >ans;
+        vector<int> v;
+        helper(root, v, ans, targetSum);
+        return ans;
+    }
+}
+*/
