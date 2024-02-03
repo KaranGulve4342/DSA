35eeccbeb5ef1c2a1c79565dfc2701750b7e45b0
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-03 08:03:26 +0530

    Decimal Equivalent of Binary Linked List.cpp code added

--- BInary Search Trees/Delete Node in a BST.cpp
+++ BInary Search Trees/Delete Node in a BST.cpp
@@ -1,14 +1,14 @@
 //LEETCODE 450
 
 /*
-TreeNode* iop(ListNode* root){
+TreeNode* iop(TreeNode* root){
     TreeNode* pred = root->left;
     while(pred->right != NULL){
         pred = pred->right;
     }
     return pred;
 }
-TreeNode* ios(ListNode* root){
+TreeNode* ios(TreeNode* root){
     TreeNode* suc = root->right;
     while(suc->left != NULL){
         suc = suc->left;
@@ -44,5 +44,62 @@ TreeNode* deleteNode(TreeNode* root,int key){
     }
     return root;
 };
+
+*/
+
+// LEETCODE 450
+
+/*
+TreeNode* iop(TreeNode* root){
+    TreeNode* pred = root->left;
+    while(pred->right != NULL){
+        pred = pred->right;
+    }
+    return pred;
+}
+
+TreeNode* ios(TreeNode* root){
+    TreeNode* succ = root->right;
+    while(root->left != NULL){
+        succ = succ->left;
+    }
+    return succ;
+}
+
+TreeNode* dleteNode(TreeNOde* root, int key){
+    if(root == NULL) return NULL;
+    if(root->val == key){
+
+        // case 1 : no child
+        if(root->left == NULL && root->right == NULL){
+            return NULL:
+        }
+
+        // case 2 : one child
+        if(root->left == NULL || root->right == NULL){
+            if(root->left != NULL) return root->left;
+            else return root->right;
+        }
+
+        // case 3 : two child
+        if(root->left != NULL && root->right != NULL){
+            // Replace the root with its inorder pred/sec 
+            // after replacing delete the pred/succ
+
+            TreeNode* pred = iop(root);
+            root->val = pred;
+            root->left = deleteNode(root->left, pred->val);
+        }
+    }
+    else if(root->val > key){
+        // go left
+        root->left = deleteNode(root->left, key);
+    }
+    else{
+        // root->val < key : go right
+        root->right = deleteNode(root->right, key);
+    }
+    return root;
+}
 
 */
