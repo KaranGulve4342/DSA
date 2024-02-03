3f91ff9d132cd909e007c6037b66e43795d091ea
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-03 10:05:07 +0530

    Find K closest elements

--- Heap/Find K Closest Elements.cpp
+++ Heap/Find K Closest Elements.cpp
@@ -23,7 +23,35 @@ public:
         }
 
         sort(ans.begin(), ans.end());
-
         return ans;
     }
 };
+/*
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int n = arr.size();
+
+        priority_queue<pair<int, int> > pq;
+        for(int i = 0;i < n;i++){
+            int diff = abs(arr[i] - x);
+
+            pq.push({diff, arr[i]});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+
+        vector<int> ans;
+        while(!pq.empty()){
+            auto p = pq.top();
+            pq.pop();
+            ans.push_back(p.second);
+        }
+
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+*/
