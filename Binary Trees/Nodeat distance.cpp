2875f18b0827b2ac26f24e0f6afb84e27e1d93cf
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-06 08:40:28 +0530

    Node at distance

---
+++ Binary Trees/Nodeat distance.cpp
@@ -0,0 +1,154 @@
+// GFG POTD 6 feb 2024
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Node Structure
+/*struct Node
+{
+	int k;
+	Node *left, *right;
+};*/
+
+class Solution
+{
+private:
+    unordered_set<Node*> set;
+
+    void dfs(Node* root, int k) {
+        if (root == nullptr) return;
+        dfs(root->left, k);
+        if (isValid(root, k))
+            set.insert(root);
+        dfs(root->right, k);
+    }
+
+    bool isValid(Node* root, int k) {
+        if (root == nullptr) return false;
+        if (root->left == nullptr && root->right == nullptr) {
+            return k == 0;
+        }
+        return isValid(root->left, k - 1) || isValid(root->right, k - 1);
+    }
+
+public:
+    int printKDistantfromLeaf(Node* root, int k) {
+        set.clear();
+        dfs(root, k);
+        return set.size();
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s, ks;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, ks);
+        int k=stoi(ks);
+        Solution obj;
+        cout<<obj.printKDistantfromLeaf(root, k)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
