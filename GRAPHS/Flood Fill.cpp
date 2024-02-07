8d22e49e200059441db2903f3ed89848dbf2434f
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-07 19:47:38 +0530

    Flood Fill code added

--- GRAPHS/Flood Fill.cpp
+++ GRAPHS/Flood Fill.cpp
@@ -1,5 +1,35 @@
 // LEETCODE 733
 
+/*
+class Solution {
+public:
+    void dfs(int i, int j, int initialColor, int color, vector<vector<int> >&image){
+        int n = image.size();
+        int m = image[0].size();
+
+        if(i < 0 || j < 0 || i >= n || j >= m) return;
+        if(image[i][j] != initialColor) return;
+
+        image[i][j] = color;
+
+        dfs(i+1, j, initialColor, color, image);
+        dfs(i-1, j, initialColor, color, image);
+        dfs(i, j+1, initialColor, color, image);
+        dfs(i, j-1, initialColor, color, image);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initialColor = image[sr][sc];
+        if(initialColor != color){
+            dfs(sr, sc, initialColor, color, image);
+        }
+        return image;
+    }
+};
+*/
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
