c2789e2f40c5380c11fa7bd14c8c888570f54ebf
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-11 00:08:40 +0530

    BFS_traversal_in_graph.cpp code added

--- GRAPHS/BFS_traversal_in_graph.cpp
+++ GRAPHS/BFS_traversal_in_graph.cpp
@@ -1,4 +1,55 @@
 // Coding Ninjas
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+
+vector<int>g[N];
+int vis[N];
+int level[N];
+
+void bfs(int source){
+    queue<int> q;
+    q.push(source);
+    vis[source] = 1;
+
+    while(!q.empty()){
+        int currVertex = q.front();
+        q.pop();
+        cout<<currVertex<<" ";
+        for(int child : g[currVertex]){
+            if(!vis[child]){
+                q.push(child);
+                vis[child] = 1;
+                level[child] = level[currVertex] + 1;
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0;i < n-1;i++){
+        int x, y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    bfs(1);
+
+    cout<<endl;
+
+    for(int i = 1;i <= n;i++){
+        cout<<i<<" "<<level[i]<<endl;
+    }
+
+    return 0;
+}
+
+
+
 
 /*
 
