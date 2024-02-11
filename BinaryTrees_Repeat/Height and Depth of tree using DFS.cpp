d6bc4447b0b697761d830a01a118ca5d1eb8a165
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-11 10:33:01 +0530

    Cherry pickup II code added

---
+++ BinaryTrees_Repeat/Height and Depth of tree using DFS.cpp
@@ -0,0 +1,23 @@
+// HEIGHT AND DEPTH OF A TREE USING DFS
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+vector<int>g[N];
+
+void dfs(int vertex){
+    // Take action on vertex after entering the vertex
+    vis[vertex] = true;
+    for(int child : g[vertex]){
+        // Take action on child before entering the child node
+        if(vis[child]) continue;
+        dfs(child);
+        // Take action on child after exiting child node
+    }
+}
+
+int main(){
+    
+    return 0;
+}
