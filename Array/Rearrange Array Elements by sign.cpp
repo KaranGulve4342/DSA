d0ce4c3ea078c538af72e5d7fc225b21f78b48a0
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-14 07:32:34 +0530

    Rearrange array element by signn code added

--- Array/Rearrange Array Elements by sign.cpp
+++ Array/Rearrange Array Elements by sign.cpp
@@ -3,12 +3,13 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> ans(nums.size(), 0);
+        int n = nums.size();
 
+        vector<int> ans(n, 0);
         int j = 0;
         int k = 1;
 
-        for(int i = 0;i < nums.size();i++){
+        for(int i = 0;i < n;i++){
             if(nums[i] > 0){
                 ans[j] = nums[i];
                 j+=2;
