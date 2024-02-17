1359e388627304a45fe2e7d929ccbe54ed425d97
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-17 07:04:07 +0530

    Furthest Building you can reach

---
+++ Heap/Furthest Building you can Reach.cpp
@@ -0,0 +1,30 @@
+// LEETCODE 1642
+
+class Solution {
+public:
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        priority_queue<int> pq;
+
+        int i = 0;
+        int difference = 0;
+        for(i = 0;i < heights.size() - 1;i++){
+            difference = heights[i+1] - heights[i];
+
+            if(difference <= 0){
+                continue;
+            }
+
+            bricks -= difference;
+            pq.push(difference);
+
+            if(bricks < 0){
+                bricks += pq.top();
+                pq.pop();
+                ladders--;
+            }
+
+            if(ladders < 0) break;
+        }
+        return i;
+    }
+};
