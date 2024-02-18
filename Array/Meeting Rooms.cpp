6d0373275555b29bcccb234af2289b7a9c36bc10
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-18 10:58:40 +0530

    Meeting Rooms code added

---
+++ Array/Meeting Rooms.cpp
@@ -0,0 +1,20 @@
+// LEETCODE 252
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool canAttend(vector<vector<int>>& intervals) {
+    if(intervals.empty()) return true;
+    for(int i = 1; i < intervals.size(); i++) {
+        if(intervals[i][0] < intervals[i - 1][1])
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    vector<vector<int>> intervals = {{0, 3}, {5, 10}, {15, 20}};
+    cout << (canAttend(intervals) ? "true" : "false") << endl;
+    return 0;
+}
