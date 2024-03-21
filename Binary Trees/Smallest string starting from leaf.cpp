41c001335b2a72377ec6a073458cb87fec20b8a3
KaranGulve4342 <karangulve4342@gmail.com>
2024-03-21 07:24:47 +0530

    Smallest string starting from leaf code added

---
+++ Binary Trees/Smallest string starting from leaf.cpp
@@ -0,0 +1,30 @@
+// LEETCODE 988
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string ans = "~";
+    void solve(TreeNode* root, string s){
+        if(root == NULL) return;
+        s = char(root->val + 'a') + s;
+        if(root->left == NULL && root->right == NULL){
+            ans = min(ans, s);
+        }
+        solve(root->left, s);
+        solve(root->right, s);
+    }
+    string smallestFromLeaf(TreeNode* root) {
+        solve(root, "");
+        return ans;
+    }
+};
