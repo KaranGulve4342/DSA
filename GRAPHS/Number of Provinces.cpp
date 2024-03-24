a3bebf82472f982e94824c582620fddaa98711fe
KaranGulve4342 <karangulve4342@gmail.com>
2024-03-24 10:12:08 +0530

    Number of Provinces code added

--- GRAPHS/Number of Provinces.cpp
+++ GRAPHS/Number of Provinces.cpp
@@ -33,3 +33,28 @@ class Solution{
         return count;
     }
 }
+
+class Solution {
+public:
+    void dfs(vector<vector<int>>& isConnected, vector<bool>& visited, int i){
+        visited[i] = true;
+        for(int j = 0;j < isConnected.size();j++){
+            if(isConnected[i][j] == 1 && !visited[j]){
+                dfs(isConnected, visited, j);
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+
+        int ans = 0;
+        for(int i = 0;i < n;i++){
+            if(!visited[i]){
+                dfs(isConnected, visited, i);
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
