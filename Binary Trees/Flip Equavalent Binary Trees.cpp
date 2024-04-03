22abf27f618d0a701003697f89b97dd5710de9fa
KaranGulve4342 <karangulve4342@gmail.com>
2024-04-03 07:35:07 +0530

    Flip Equivalent Binary Trees code added

---
+++ Binary Trees/Flip Equavalent Binary Trees.cpp
@@ -0,0 +1,34 @@
+// LEETCODE 951
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if(root1 == NULL && root2 == NULL){
+            return true;
+        }
+        if(root1 == NULL && root2 != NULL){
+            return false;
+        }
+        if(root1 != NULL && root2 == NULL){
+            return false;
+        }
+
+        if(root1->val != root2->val) return false;
+
+        // root2 = invertTree(root2);
+        bool ans = (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right)) || 
+                    (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+        return ans;
+    }
+};
