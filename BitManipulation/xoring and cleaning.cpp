a53045e3372bc3d512fa28d6784c6d6763782d96
KaranGulve4342 <karangulve4342@gmail.com>
2024-04-14 00:21:00 +0530

    Xoring and cleaning code added

---
+++ BitManipulation/xoring and cleaning.cpp
@@ -0,0 +1,59 @@
+// GFG POTD
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void printArr(int n, int arr[]) {
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    // Method to set all elements of the array to zero
+    void setToZero(int n, int arr[]) {
+        for(int i = 0;i < n;i++){
+            arr[i] = 0;
+        }
+    }
+
+    // Method to calculate XOR of each element with its index
+    void xor1ToN(int n, int arr[]) {
+        for (int i = 0; i < n; i++) {
+            arr[i] ^= i;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        obj.xor1ToN(n, arr);
+        obj.printArr(n, arr);
+        obj.setToZero(n, arr);
+        obj.printArr(n, arr);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
