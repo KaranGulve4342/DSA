a9d89c1ca17726ef2fa9733584792a2c7bb4fb1c
KaranGulve4342 <karangulve4342@gmail.com>
2024-04-28 06:56:50 +0530

    Number of Burgers with no waste of Ingredients code added

---
+++ Math/Number of Burgers with no waste of Ingredients.cpp
@@ -0,0 +1,11 @@
+// LEETCODE 1276
+
+class Solution {
+public:
+    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        if(tomatoSlices % 2 == 0 && cheeseSlices <= tomatoSlices/2 && tomatoSlices <= cheeseSlices * 4){
+            return {(tomatoSlices/2) - cheeseSlices, (2*cheeseSlices) - (tomatoSlices/2)};
+        }
+        return {};
+    }
+};
