48a2a8d1944f396dffab8ad91d99f3f1d59f07b9
KaranGulve4342 <karangulve4342@gmail.com>
2024-05-01 08:05:41 +0530

    Count Number of Maximum Bitwise-OR subsets.cpp

---
+++ BitManipulation/Count Number of Maximum Bitwise-OR Subsets.cpp
@@ -0,0 +1,27 @@
+// LEETCODE 2044
+
+class Solution {
+public:
+    void solve(int currIdx, vector<int>& nums, int &maxOR, int currOR, int &ans){
+        if(currIdx == nums.size()){
+            if(currOR == maxOR){
+                ans++;
+            }
+            return;
+        }
+
+        solve(currIdx + 1, nums, maxOR, currOR, ans);
+        solve(currIdx + 1, nums, maxOR, currOR | nums[currIdx], ans);
+    }
+    int countMaxOrSubsets(vector<int>& nums) {
+        int maxOR = 0;
+        int ans = 0;
+
+        for(auto num : nums){
+            maxOR |= num;
+        }
+
+        solve(0, nums, maxOR, 0, ans);
+        return ans;
+    }
+};
