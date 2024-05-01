8c19453def41e95dde04ed3d11bca5ef1af19c3e
KaranGulve4342 <karangulve4342@gmail.com>
2024-05-01 07:37:28 +0530

    REverse prefix of word code added

---
+++ strings/Reverse Prefix of word.cpp
@@ -0,0 +1,16 @@
+// LEETCODE 2000
+
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index = 0;
+        for(int i = 0;i < word.length();i++){
+            if(word[i] == ch){
+                index = i;
+                break;
+            }
+        }
+        reverse(word.begin(), word.begin() + index+1);
+        return word;
+    }
+};
