c07ad6176546ba1d1535f3a29381647d00304185
KaranGulve4342 <karangulve4342@gmail.com>
2024-05-02 19:41:16 +0530

    XOR queries of a subarray code added

---
+++ BitManipulation/XOR queries of a subarray.cpp
@@ -0,0 +1,26 @@
+// LEETCODE 1310
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> ans;
+        for(int i = 1;i < arr.size();i++){
+            arr[i] ^= arr[i-1];
+        }
+
+        for(int i = 0;i < queries.size();i++){
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            if(start == 0){
+                ans.push_back(arr[end]);
+            }
+            else{
+                ans.push_back(arr[start - 1] ^ arr[end]);
+            }
+        }
+
+        return ans;
+
+    }
+};
