74f2f19d2d042e0478319978f7c0fadfde068143
KaranGulve4342 <karangulve4342@gmail.com>
2024-05-02 19:56:08 +0530

    Sum of all subset XOR Totals code added

---
+++ BitManipulation/Sum of all subset XOR equals.cpp
@@ -0,0 +1,21 @@
+// LEETCODE 1863
+
+class Solution {
+public:
+    int ans;
+    void solve(vector<int>& nums, int idx, int curr){
+        if(idx == nums.size()){
+            ans += curr;
+            return;
+        }
+
+        solve(nums, idx + 1, curr);
+        solve(nums, idx + 1, curr ^ nums[idx]);
+    }
+    int subsetXORSum(vector<int>& nums) {
+        ans = 0;
+
+        solve(nums, 0, 0);
+        return ans;
+    }
+};
