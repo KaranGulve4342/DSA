513e27f7dbde34b474fbe91e57b9db33adf0d7db
KaranGulve4342 <karangulve4342@gmail.com>
2024-05-04 08:14:03 +0530

    Gray Code code added

---
+++ BitManipulation/Gray Code.cpp
@@ -0,0 +1,16 @@
+// LEETCODE 89
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans(1 << n);
+
+        for(int i = 0;i < (1 << n);i++){
+            ans[i] = i ^ (i >> 1);
+        }
+        return ans;
+    }
+};
