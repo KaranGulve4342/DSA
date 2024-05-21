4674ec1c28d98ee0deb71d914544d6f712e270b9
KaranGulve4342 <karangulve4342@gmail.com>
2024-05-21 09:51:20 +0530

    Special array I code added

---
+++ Array/Special array I.cpp
@@ -0,0 +1,18 @@
+// LEETCODE 3151
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i = 1;i < n;i++){
+            if(nums[i-1]%2 == nums[i]%2){
+                return false;
+            }
+        }
+        return true;
+    }
+};
