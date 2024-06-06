ca2f3c99e542ef313282273e49828e9754c1a332
KaranGulve4342 <karangulve4342@gmail.com>
2024-06-06 12:14:37 +0530

    Two Permuatation code added

--- beginning/Two Permulation.cpp
+++ beginning/Two Permulation.cpp
@@ -9,47 +9,23 @@ int main(){
     long long int t;
     cin>>t;
     for(long long int i=0;i<t;i++){
-        long long int n,a,b;
+        int n,a,b;
         cin>>n>>a>>b;
-        if(n==1){
-            if(a==1 && b==1){
-                cout<<"YES"<<endl;
+        if(a+b>=n){
+            if(a==n && b==n){
+                cout<<"Yes"<<endl;
             }
             else{
-                cout<<"NO"<<endl;
-            }
-        }
-        else if(n==2){
-            if(a==b && (a==1 || a==2)){
-                cout<<"NO"<<endl;
-            }
-            else if(a>n || b>n){
-                cout<<"NO"<<endl;
-            }
-            else{
-                cout<<"NO"<<endl;
-            }
-        }
-        else if(n==3){
-            if(a==b && (a==1 || a==2 || a==3)){
-                cout<<"NO"<<endl;
-            }
-            else if(a>n || b>n){
-                cout<<"NO"<<endl;
-            }
-            else{
-                cout<<"NO"<<endl;
+                cout<<"No"<<endl;
             }
         }
         else{
-            if(a==b){
-                cout<<"NO"<<endl;
-            }
-            else if(a>n || b>n){
-                cout<<"NO"<<endl;
+            int temp=n-a-b;
+            if(temp>1){
+                cout<<"Yes"<<endl;
             }
             else{
-                cout<<"YES"<<endl;
+                cout<<"No"<<endl;
             }
         }
     }
