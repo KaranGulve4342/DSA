0584863fe3fa9556d2e52b668126d11e501943e9
KaranGulve4342 <karangulve4342@gmail.com>
2024-06-12 07:34:56 +0530

    GFG and Leetcode POTD's code added

--- Sorting/Sort colors.cpp
+++ Sorting/Sort colors.cpp
@@ -3,6 +3,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        int lo = 0;
+        int mid = 0;
+        int hi = nums.size() - 1;
+
+        while(mid <= hi){
+            if(nums[mid] == 0){
+                swap(nums[lo], nums[mid]);
+                lo++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[hi]);
+                hi--;
+            }
+        }
     }
 };
