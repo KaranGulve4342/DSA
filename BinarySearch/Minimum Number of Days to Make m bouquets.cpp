3959af7729a505d24201865de944a9e788030e3f
KaranGulve4342 <karangulve4342@gmail.com>
2024-06-19 07:52:10 +0530

    Minimum Number of Days to make m bouqeuests code added

---
+++ BinarySearch/Minimum Number of Days to Make m bouquets.cpp
@@ -0,0 +1,41 @@
+// LEETCODE 1482
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+        int left = 1;
+        int right = INT_MAX;
+
+        if((long long)m * (long long)k > (long long)n){
+            return -1;
+        }
+
+        while(left < right){
+            int mid = left + (right - left)/2;
+            int flow = 0;
+            int bouq = 0;
+
+            for(int j = 0;j < n;j++){
+                if(bloomDay[j] > mid){
+                    flow = 0;
+                }
+                else if(++flow >= k){
+                    bouq++;
+                    flow = 0;
+                }
+            }
+
+            if(bouq < m){
+                left = mid + 1;
+            }
+            else{
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
