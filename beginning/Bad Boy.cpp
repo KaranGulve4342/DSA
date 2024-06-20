62943887c098d14e3108217ccd459d92b317e660
KaranGulve4342 <karangulve4342@gmail.com>
2024-06-20 11:20:48 +0530

    Integral Points inside Triangle code added

---
+++ beginning/Bad Boy.cpp
@@ -0,0 +1,16 @@
+// CODEFORCES 900 RATED
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout<<1<<" "<<1<<" "<<n<<" "<<m<<"\n";
+    }
+
+    return 0;
+}
