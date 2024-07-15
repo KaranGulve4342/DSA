6833e2c6500c22517ac3119307afdfe6f33d3108
KaranGulve4342 <karangulve4342@gmail.com>
2024-07-15 10:10:59 +0530

    Index of first occurrence of a pattern in a text code added

---
+++ strings/Index of first occurrence of a pattern in a text.cpp
@@ -0,0 +1,42 @@
+// GFG
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMatching(string text, string pat) {
+        // Code here
+        int n = text.length();
+        int m = pat.length();
+
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && text[i + j] == pat[j]) {
+                j++;
+            }
+            if (j == m) {
+                return i;  // pattern found at index i
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string text, pat;
+        cin >> text >> pat;
+        Solution obj;
+        cout << obj.findMatching(text, pat) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
