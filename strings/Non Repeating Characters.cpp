a8b2b300022d8a4848c23eaa55c9db3e2390def8
KaranGulve4342 <karangulve4342@gmail.com>
2024-08-08 17:52:11 +0530

    Generate IP Addresses

---
+++ strings/Non Repeating Characters.cpp
@@ -0,0 +1,62 @@
+// GFG PRACTICE
+
+// EVERY PROBLEM IS SOLVABLE
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the first non-repeating character in a string.
+    char nonrepeatingCharacter(string S)
+    {
+       //Your code here
+       unordered_map<char, int> umap;
+       for(auto ch : S){
+           umap[ch]++;
+       }
+       
+       for(int i = 0;i < S.length();i++){
+           if(umap[S[i]] == 1){
+               return S[i];
+           }
+       }
+       
+       return '$';
+       
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--)
+	{
+	
+	    string S;
+	    cin >> S;
+	    Solution obj;
+        char ans = obj.nonrepeatingCharacter(S);
+        
+        if(ans != '$')
+	    cout << ans;
+        else cout << "-1";
+            
+        cout << endl;
+	    
+	}
+	
+	return 0;
+}
+
+// } Driver Code Ends
