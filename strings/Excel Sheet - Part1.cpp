484fa2aabf64db1784456b4096b152eab0fdb85c
KaranGulve4342 <karangulve4342@gmail.com>
2024-08-12 10:09:59 +0530

    Excel sheet Part 1 code added

---
+++ strings/Excel Sheet - Part1.cpp
@@ -0,0 +1,43 @@
+// GFG PRACTICE
+
+//{ Driver Code Starts
+//Initial template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+    string ExcelColumn(int N)
+    {
+        string ans = "";
+        while(N){
+            N--; 
+            char x = (N % 26) + 'A'; 
+            ans = x + ans;
+            N /= 26;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<<ob.ExcelColumn(n)<<endl;
+    }
+    return 0;
+}
+    
+// } Driver Code Ends
