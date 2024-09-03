b6ff46a6081e1b7250c3b048585af97fef939ce1
KaranGulve4342 <karangulve4342@gmail.com>
2024-09-03 11:25:15 +0530

    Number of distinct islands code added

--- GRAPHS/Number of Distinct Islands.cpp
+++ GRAPHS/Number of Distinct Islands.cpp
@@ -1,93 +1,71 @@
-// GFG
+// GFG PRACTICE
 
-#include<bits/stdc++.h>
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
 using namespace std;
 
+
+// } Driver Code Ends
+// User function Template for C++
+
 class Solution {
-    void dfs(int row, int col, vector<vector<int> > &vis, vector<vector<int> >&grid, vector<pair<int, int> >&vec, int row0, int col0){
-        vis[row][col] = 1;
-        vec.push_back({row - row0, col - col0});
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, -1, 0, +1};
-
-        for(int i = 0;i < 4;i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
-                dfs(nrow, ncol, vis, grid, vec, row0, col0);
-            }
+  public:
+    void dfs(int i, int j, vector<vector<int>>& grid, string&s, string direction){
+        if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size()){
+            return;
         }
+        
+        if(grid[i][j] == 0 || grid[i][j] == 2){
+            return;
+        }
+        
+        grid[i][j] = 2;
+        
+        s += direction;
+        
+        dfs(i+1, j, grid,s, "r");
+        dfs(i, j+1, grid,s, "d");
+        dfs(i-1, j, grid,s, "u");
+        dfs(i, j-1, grid,s, "l");
+        
+        s += "b";
     }
-
-    int countDistinctIslands(vector<vector<int> >& grid){
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int> > vis(n, vector<int> (m, 0));
-        set<vector<pair<int, int> > > st;
-
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
-                if(!vis[i][j] && grid[i][j] == 1){
-                    vector<pair<int, int> > vec;
-
-                    dfs(i, j, grid, vis, vec, i, j);
-                    st.insert(vec);
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        
+        unordered_set<string> ans;
+        for(int i = 0;i < grid.size();i++){
+            for(int j = 0;j < grid[0].size();j++){
+                if(grid[i][j] == 1){
+                    string t = "";
+                    dfs(i, j, grid, t, "o");
+                    ans.insert(t);
                 }
             }
         }
-
-        return st.size();
+        
+        return ans.size();
     }
-}
+};
 
-/*
-class Solution{
-public:
-    void dfs(int row, int col, vector<vector<int> > &vis, vector<vector<int> >&grid, vector<pair<int, int> > &vec, int row0, int col0){
-        vis[row][col] = 1;
 
-        vec.push_back({row - row0, col - col0});
+//{ Driver Code Starts.
 
-        int n = grid.size();
-        int m = grid[0].size();
+int main() {
 
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1};
-
-        for(int i = 0;i < 4;i++){
-            int nrow = row + delRow[i];
-            int ncol=  col + delCol[i];
-
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
-                dfs(nrow, ncol, vis, grid, vec, row0, col0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
             }
         }
-    }
-
-    int countDistinctIslands(vector<vector<int> >& grid){
-        int n = grid.size();
-        int m = grid[0].size();
-
-        vector<vector<int> >vis(n, vector<int> (m, 0));
-        set<vector<pair<int, int> > > st;
-
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < m;j++){
-                if(!vis[i][j] && grid[i][j] == 1){
-                    vector<pair<int, int> > vec;
-                    dfs(i, j, vis, grid, vec, i, j);
-                    st.insert(vec);
-                }
-            }
-        }
-
-        return st.size();
+        Solution obj;
+        cout << obj.countDistinctIslands(grid) << endl;
     }
 }
-*/
+// } Driver Code Ends
