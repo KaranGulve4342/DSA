75a6272004f8b92c6514dee3a9b99339aa2bb732
KaranGulve4342 <karangulve4342@gmail.com>
2024-09-06 07:39:54 +0530

    Maximum Index code added

--- Array/Maximum Index.cpp
+++ Array/Maximum Index.cpp
@@ -68,3 +68,56 @@ int main()
     return 0;
 } 
 // } Driver Code Ends
+
+
+
+// METHOD II
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int maxIndexDiff(int arr[], int n) {
+        vector<int> mini(n, 0);
+        vector<int> maxi(n, 0);
+        mini[0] = arr[0];
+        maxi[n-1] = arr[n-1];
+        for(int i = 1;i < n;i++){
+            mini[i] = min(mini[i-1], arr[i]);
+            maxi[n-i-1] = max(maxi[n-i], arr[n-i-1]);
+        }
+        int ans = 0;
+        int i = 0;
+        int j = 0;
+        while(i < n && j < n){
+            if(mini[i] <= maxi[j]){
+                ans = max(ans, j-i);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        
+        return ans;
+    }
+
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.maxIndexDiff(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
