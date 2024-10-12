3c30198e94526fc484a838fe1d1439ac24d60d8f
KaranGulve4342 <karangulve4342@gmail.com>
2024-10-12 11:52:09 +0530

    Two smallest in every subarray code added

---
+++ Array/Two Smallest in every subarray.cpp
@@ -0,0 +1,48 @@
+// GFG POTD 12TH OCTOBER 2024
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int pairWithMaxSum(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        if(n<=1){
+            return -1;
+        }
+        int sum=0;
+        for(int i=0;i<n-1;i++){
+            sum=max(sum,arr[i]+arr[i+1]);
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore();
+
+    while (tc--) {
+        string input;
+        getline(cin, input);
+
+        stringstream ss(input);
+        vector<int> a;
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        Solution obj;
+        int res = obj.pairWithMaxSum(a);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
