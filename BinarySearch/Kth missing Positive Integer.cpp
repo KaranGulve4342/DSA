e982e80944e8ad04974e8b51ddc69fedcad9692c
KaranGulve4342 <karangulve4342@gmail.com>
2024-10-17 00:21:19 +0530

    Kth Missing Positive Integer code added

---
+++ BinarySearch/Kth missing Positive Integer.cpp
@@ -0,0 +1,25 @@
+// LEETCODE 1539
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        int lo = 0;
+        int hi = n-1;
+
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+
+            int missing = arr[mid] - (mid+1);
+
+            if(missing < k){
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+
+        return k + hi + 1;
+    }
+};
