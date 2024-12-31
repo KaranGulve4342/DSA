d9d0c9e7e0decde7a627aefea623f41901f4adad
KaranGulve4342 <karangulve4342@gmail.com>
2024-12-31 09:36:25 +0530

    Refactor Minimum Cost for Tickets solution to use dynamic programming with memoization and improve readability

--- DYNAMIC PROGRAMMING/Minimum Cost for tickets.cpp
+++ DYNAMIC PROGRAMMING/Minimum Cost for tickets.cpp
@@ -1,121 +1,34 @@
-// CODING NINJAS
-
-// USING RECURSION
-/*
-int solve(int n, vector<int>& days, vector<int>& cost, int index) {
-    // base case
-    if(index >= n){
-        return 0;
-    }
-
-    // 1 day pass
-    int option1 = cost[0] + solve(n, days, cost, index+1);
-
-    int i;
-    // 7 day pass
-    for(i = index;i < n && days[i] < days[index] + 7;i++);
-
-    int option2 = cost[1] + solve(n, days, cost, i);
-
-    // 30 days pass
-    for(i = index;i < n && days[i] < days[index] + 30;i++);
-
-    int option3 = cost[2] + solve(n, days, cost, i);
-
-    return min(option1, min(option2, option3));
-}
-int minimumCoins(int n, vector<int> days, vector<int> cost){
-    return solve(n, days, cost, 0);
-}
-*/
-
-// USING MEMIOSATION
-/*
-int solveMem(int n, vector<int>& days, vector<int>& cost, int index, vector<int>& dp){
-    if(index >= n){
-        return 0;
-    }
-
-    // 1 day pass
-    int option1 = cost[0] + solveMem(n, days, cost, index+1, dp);
-
-    int i;
-    // 7 day pass
-    for(i = index;i < n && days[i] < days[index] + 7;i++);
-
-    int option2 = cost[1] + solveMem(n, days, cost, i, dp);
-
-    // 30 day pass
-    for(i = index, i < n && days[i] < days[index] + 30;i++);
-
-    int option3 = cost[2] + solvemem(n, days, cost, i, dp);
-
-    dp[index] = min(option1, min(option2, option3));
-
-    return dp[index];
-}
-int minimumCoins(int n, vector<int> days, vector<int> cost){
-    vector<int> dp(n+1, -1);
-    return solve(n, days, cost, 0, dp);
-}
-*/
-
-// USING TABULATION
-/*
-int solveTab(int n, vector<int>& days, vector<int>& cost){
-    vector<int> dp(n+1, INT_MAX);
-    dp[n] = 0;
-
-    for(int k = n-1;k >= 0;k--) {
-        // 1 day pass
-        int option1 = cost[0] + dp[k+1];
-
-        int i;
-        // 7 days pass
-
-        for(i = k;i < n && days[i] < days[k] + 7;i++);
-
-        int option2 = cost[1] + dp[i];
-
-        // 30 days pass
-        for(int i = k;i < n && days[i] < days[k] + 30; i++);
-
-        int option3 = cost[2] + dp[i];
-
-        dp[k] = min(option1, min(option2, option2));
-    }
-    return dp[0];
-}
-int minimumCoins(int n, vector<int> days, vecotr<int> cost){
-    return solveTab(n, days, cost);
-}
-*/
-
-// USING QUEUE
-
-/*
-int minimumCoins(int n, vector<int> days, vector<int> cost){
-    int ans = 0;
-
-    queue<pair<int, int> >month;
-    queue<pair<int, int> >week;
-
-    for(int day : days){
-        // step1 : remove expired days
-        while(!month.empty() && month.front().first + 30 <= day){
-            month.pop();
-        }
-        while(!week.empty() && week.front().first + 7 <= day){
-            week.pop();
+class Solution {
+public:
+    unordered_set<int> st;
+    int solve(vector<int>& days, vector<int>&costs, vector<int>& dp, int currDay){
+        if(currDay > days[days.size() - 1]){
+            return 0;
         }
 
-        // step2 : add cost for current day
-        week.push(make_pair(day, ans+cost[1]));
-        month.push(make_pair(day, ans+cost[2]));
+        if(st.find(currDay) == st.end()){
+            return solve(days, costs, dp, currDay + 1);
+        }
 
-        // step3 : ans update
-        ans = min(ans + cost[0], min(week.front().second, month.front().second));
+        if(dp[currDay] != -1){
+            return dp[currDay];
+        }
+
+        int oneDay = costs[0] + solve(days, costs, dp, currDay + 1);
+        int sevenDay = costs[1] + solve(days, costs, dp, currDay + 7);
+        int thirtyDay = costs[2] + solve(days, costs, dp, currDay + 30);
+
+        return dp[currDay] = min({oneDay, sevenDay, thirtyDay});
     }
-    return ans;
-}
-*/
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        int lastDay = days[days.size() - 1];
+        vector<int> dp(lastDay + 1, -1);
+
+        for(auto day : days){
+            st.insert(day);
+        }
+
+        return solve(days, costs, dp, 1);
+
+    }
+};
